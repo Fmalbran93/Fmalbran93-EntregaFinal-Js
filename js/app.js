616753192054219417fd708a855250b205fde93e
fmalbran93 <fmalbran93@gmail.com>
2023-01-08 17:35:05 -0300

    arreglos

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const dataFetch = async () => {
     try {
-        const res = await fetch('/js/database.json')
+        const res = await fetch('js/database.json')
         const data = await res.json()
         // console.log(data);
         MostrarCard(data);
